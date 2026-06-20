ef5a9156acdea2480c170142cacb73ac851aa3b8
Boris Damevin <borisdamevin@gmail.com>
2026-06-20 23:38:15 +0200

    ci: migrate release config to `release.config.js`

---
+++ release.config.js
@@ -0,0 +1,71 @@
+// ============================================================================================= //
+//                                            RELEASE                                            //
+// ============================================================================================= //
+
+export default {
+    branches: [
+        'main',
+        '+([0-9])?(.{+([0-9]),x}).x',
+        {
+            name: 'beta',
+            prerelease: true
+        }
+    ],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'conventionalcommits',
+                releaseRules: [
+                    {
+                        type: 'build',
+                        scope: 'deps-dev',
+                        release: false
+                    },
+                    {
+                        type: 'build',
+                        scope: 'deps',
+                        release: 'minor'
+                    }
+                ]
+            }
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'conventionalcommits',
+                presetConfig: {
+                    types: [
+                        {
+                            type: 'feat',
+                            section: 'Features'
+                        },
+                        {
+                            type: 'fix',
+                            section: 'Bug Fixes'
+                        },
+                        {
+                            type: 'chore',
+                            scope: 'deps',
+                            section: 'Dependencies'
+                        }
+                    ]
+                }
+            }
+        ],
+        [
+            '@semantic-release/changelog',
+            {
+                changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file. See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.'
+            }
+        ],
+        '@semantic-release/npm',
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                message: 'chore(release): publish version ${nextRelease.version}'
+            }
+        ]
+    ]
+};
